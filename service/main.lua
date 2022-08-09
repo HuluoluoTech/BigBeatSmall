115bd79e62d54029f231079a7def987b2495ebef
zTgx <747674262@qq.com>
2022-08-09 18:00:36 +0800

    fomat main.lua

--- service/main.lua
+++ service/main.lua
@@ -2,47 +2,60 @@
 * service 的入口函数
 ]]
 
+-- import 必要的 package
 local skynet = require "skynet"
 local skynet_manager = require "skynet.manager"
 local cluster = require "skynet.cluster"
 local config_run = require "config_run"
 
+--[[
+* 剥离一个function出来，目前直接closure看着不太舒服，比较乱的感觉
+* 执行逻辑
+]] 
 function run()
-	--初始化
-	local mynode = skynet.getenv("node")
-	local nodecfg = config_run[mynode]
+	--初始化当前Node
+	local current_node = skynet.getenv("node")
+	local node_cfg = config_run[current_node]
+
 	--节点管理
-	local nodemgr = skynet.newservice("nodemgr","nodemgr", 0)
-	skynet.name("nodemgr", nodemgr)
+	local node_mgr = skynet.newservice("nodemgr","node_mgr", 0)
+	skynet.name("node_mgr", node_mgr)
+
 	--集群
 	cluster.reload(config_run.cluster)
-	cluster.open(mynode)
-	--gate
-	for i, v in pairs(nodecfg.gateway or {}) do
+	cluster.open(current_node)
+
+	--开启gate服务
+	for i, v in pairs(node_cfg.gateway or {}) do
 		local srv = skynet.newservice("gateway","gateway", i)
 		skynet.name("gateway"..i, srv)
 	end
-	--login
-	for i, v in pairs(nodecfg.login or {})  do
+
+	--开始login服务
+	for i, v in pairs(node_cfg.login or {})  do
 	local srv = skynet.newservice("login","login", i)
 		skynet.name("login"..i, srv)
 	end
-	--agentmgr
+
+	--开启agentmgr服务
 	local anode = config_run.agentmgr.node
-	if mynode == anode then
+	if current_node == anode then
 		local srv = skynet.newservice("agentmgr", "agentmgr", 0)
 		skynet.name("agentmgr", srv)
 	else
 		local proxy = cluster.proxy(anode, "agentmgr")
 		skynet.name("agentmgr", proxy)
 	end
-	--scene (sid->sceneid)
-	for _, sid in pairs(config_run.scene[mynode] or {}) do
+
+	--scene (sid->sceneid)服务
+	for _, sid in pairs(config_run.scene[current_node] or {}) do
 		local srv = skynet.newservice("scene", "scene", sid)
 		skynet.name("scene"..sid, srv)
 	end
-	--退出自身
+
+	--退出服务
 	skynet.exit()
 end
 
+--开启服务
 skynet.start(run)
