a1fbc88089011cafd752fa076c1488cb9821197d
zTgx <747674262@qq.com>
2022-08-12 11:01:33 +0800

    Fix python client parse chinese chars

--- tests/client.py
+++ tests/client.py
@@ -1,6 +1,6 @@
 import socket
 
-from questionary import password
+import json
 
 host = '127.0.0.1'
 port = 8001
@@ -10,19 +10,14 @@ password = "123"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
-# msg = 'hello'
-# client.send(msg.encode('utf-8'))
-# data = client.recv(1024)
-# print("服务的发来的消息：%s" %data)
-
-# 客户端 reqlogin 格式: login, userid, password#
 
 def reqlogin():
     reqdata = "login," + "1001"+","+password+"#"
     print("reqdata: ", reqdata)
     client.send(reqdata.encode('utf-8'))
     respdata = client.recv(1024)
-    print("登录返回：", respdata)
+    # print("登录返回： ", json.dumps(respdata).decode("utf-8"))
+    print("登录返回： ", respdata.decode("utf-8"))
 
 def close():
     client.close()
