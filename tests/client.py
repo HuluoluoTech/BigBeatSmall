de787f82f1c2b6adfeee5806ec01415e7f9fefa6
zTgx <747674262@qq.com>
2022-08-12 16:23:45 +0800

    Add player enter game protocol

--- tests/client.py
+++ tests/client.py
@@ -9,6 +9,15 @@ port = 8001
 
 password = "123"
 
+###
+# 通信协议：
+#
+# login 登录协议： login,playerid,password#
+# enter 进入游戏： enter#
+#
+#
+###
+
 def new_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((host, port))
@@ -25,6 +34,12 @@ def reqlogin(playerid, password):
     # print("登录返回： ", json.dumps(respdata).decode("utf-8"))
     print("登录返回： ", respdata.decode("utf-8"))
 
+    # 等一秒钟，模拟用户点击进入游戏按钮
+    sleep(1)
+
+    print("玩家 ", playerid, " 点击了 [进入游戏] 按钮")
+    enter(client)
+
     simulate_play()
     close(client)
 
@@ -36,5 +51,11 @@ def simulate_play():
 def close(client):
     client.close()
 
+def enter(client):
+    reqdata = "enter#"
+    client.send(reqdata.encode('utf-8'))
+    respdata = client.recv(1024)
+    print("进入游戏返回数据: ", respdata.decode('utf-8'))
+
 threading.Thread(target=reqlogin, args=("1001", password)).start()
-threading.Thread(target=reqlogin, args=("1002", password)).start()
+# threading.Thread(target=reqlogin, args=("1002", password)).start()
